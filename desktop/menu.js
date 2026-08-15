6f6888718c0f5577c4a2e0c3b456a20c8799fe0f
Victor Ferreira Franco <31971163+victorffrock@users.noreply.github.com>
2026-08-15 10:59:53 -0300

    Integra menu GNOME ao aplicativo desktop

---
+++ desktop/menu.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function createApplicationMenuTemplate(options) {
+  var onAction = options.onAction;
+  var onQuit = options.onQuit;
+
+  function action(label, accelerator, name) {
+    return {
+      label: label,
+      accelerator: accelerator,
+      click: function () { onAction(name); }
+    };
+  }
+
+  return [
+    {
+      label: 'Arquivo',
+      submenu: [
+        action('Adicionar boletos…', 'CommandOrControl+O', 'open-files'),
+        { type: 'separator' },
+        { label: 'Sair', role: 'quit', click: onQuit }
+      ]
+    },
+    {
+      label: 'Ajuda',
+      submenu: [
+        action('Manual', 'F1', 'manual'),
+        action('Sobre o Somador de Contas', undefined, 'about')
+      ]
+    }
+  ];
+}
+
+module.exports = { createApplicationMenuTemplate };
--- desktop/preload.js
+++ desktop/preload.js
@@ -3,5 +3,11 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('somadorDesktop', {
   isDesktop: true,
   minimize: () => ipcRenderer.send('window-minimize'),
-  close: () => ipcRenderer.send('window-close')
+  close: () => ipcRenderer.send('window-close'),
+  onAppAction: (handler) => {
+    if (typeof handler !== 'function') return () => {};
+    const listener = (_event, action) => handler(action);
+    ipcRenderer.on('somador:action', listener);
+    return () => ipcRenderer.removeListener('somador:action', listener);
+  }
 });
